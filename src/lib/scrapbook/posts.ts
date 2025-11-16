e7df7a03ebcec0afc57b08106e3baff9817488c9
Natasha Ann <natashaannlum@gmail.com>
2025-11-16 14:30:25 +0800

    feat: add scrapbook posts

--- src/lib/scrapbook/posts.ts
+++ src/lib/scrapbook/posts.ts
@@ -2,27 +2,72 @@ import { Post } from "./FeaturePage";
 
 export const scrapbookPosts: Post[] = [
   {
+    title: "Tech Podcast",
+    heroImage: "/scrapbook/posts/ragTech.png",
+    link: "https://ragtechdev.com/",
+    readMoreText: "SEE RAGTECH",
+    summary: "I co-founded ragTech, a podcast that makes emerging technology understandable without losing nuance. Through episodes, reels, blog posts, and even a tech taboo card game, I experiment with formats to communicate technical concepts to everyday audiences and spark thoughtful conversation about AI, tech careers, and more.",
+  },
+  {
+    title: "Community Building",
+    heroImage: "/scrapbook/posts/WomenDevsSG.jpeg",
+    link: "https://womendevssg.netlify.app/",
+    readMoreText: "SEE WOMENDEVSSG",
+    summary:
+      "As Partnerships Lead at Women Devs SG, I organize public events , moderate panels, and collaborate with researchers and industry experts all across tech (robotics, IOT, AI, game dev etc.) to represent and empower women in tech. This community is everything to me!"
+  },
+  {
+    title: "Communicating Software",
+    heroImage: "/scrapbook/posts/usaf.jpeg",
+    link: "https://www.airforcebes.af.mil/News/Display/Article/3610891/bespin-and-singapore-air-force-present-stargazer-at-pentagon/",
+    readMoreText: "SEE USAF ARTICLE",
+    summary:
+      "While leading engineering projects in the Republic of Singapore Air Force, I pitched and secured buy-in for AEGIS, the first in-house software product in the RSAF, and STARGAZER, a first-of-its-kind partnership product with USAF. I presented to senior officials, translating engineering strategy into compelling narratives for non-technical stakeholders."
+  },
+  {
+    title: "Content Creation",
+    heroImage: "/scrapbook/posts/reel.jpg",
+    link: "/links",
+    readMoreText: "FIND MY SOCIALS",
+    summary: "I’ve grown engaged audiences across TikTok, Instagram and LinkedIn, sharing videos and writing about AI, tech culture, and societal impact. Recently, I completed a content-creator program, learning audience design, storytelling structures, and multimedia strategy for social platforms.",
+  },
+  {
+    title: "Coding Community Apps",
+    heroImage: "/scrapbook/posts/bibsnbub.jpg",
+    link: "https://bibsnbub.vercel.app/",
+    readMoreText: "SEE BIBS&BUB",
+    summary:
+      "I design and build open-source tools for communities: 🍼 Bibs&Bub nursing room locator, 🤖 Volunteer Telebot, 📣 Content Generator for social campaigns. Each began as a storytelling challenge: identifying underserved needs and transforming them into usable, meaningful solutions.",
+  },
+      {
     title: "Digital Sustainability",
     heroImage: "/scrapbook/posts/digital-sustainability.png",
-    link: "https://example.com/kitten-fostering",
-    readMoreText: "READ STORY",
+    link: "https://www.linkedin.com/posts/natashaannn_sustainability-ai-advocate-activity-7192338064235466752-0dN7?utm_source=social_share_send&utm_medium=member_desktop_web&rcm=ACoAACvjDaEBW_Out_-5Y5l0vlGWfyWgUwJC0Ac",
+    readMoreText: "READ POST",
     summary:
-      "I started speaking at events about digital sustainability and the importance of intentional use of AI. I also talk about the importance of responsible AI use.",
+      "As a founding speaker for the first two Green IO conferences, the first tech sustainability conference in Asia, I speak at events and panels about intentional AI usage, carbon impact awareness, and sustainable design — exploring how we can sustainably build technology that aligns with societal values and human wellbeing.",
   },
   {
     title: "AI Ethics",
     heroImage: "/scrapbook/posts/ai-ethics.png",
-    link: "https://example.com/responsible-ai-storytelling",
-    readMoreText: "READ STORY",
+    link: "https://www.youtube.com/watch?v=BPYt_Xs20q8&t=1s",
+    readMoreText: "WATCH PANEL",
     summary:
-      "How I experiment with video, writing, and talks to make complex AI concepts accessible to non-technical audiences.",
+      "I've spoken at and organized events explaining AI safety, interpretability, and decision-making, helping non-technical audiences understand the stakes and possibilities. The below video is a full recording of one of my favourite panels I've organized that brought together various AI experts to disucss the challenges of regulating and navigating AI!",
   },
   {
-    title: "Women in Tech",
-    heroImage: "/scrapbook/posts/women-in-tech.png",
-    link: "https://example.com/women-in-tech",
-    readMoreText: "READ STORY",
+    title: "Research Roots",
+    heroImage: "/scrapbook/posts/ucla.JPG",
+    link: "https://international.ucla.edu/Institute/article/221262",
+    readMoreText: "READ UCLA FEATURE",
+    summary: "As a Global Studies major at UCLA and later a Master's student in Computer Information Systems at Boston University, my research sits at the intersection of technology, networks, and society. At UCLA, I worked with Dr. Leonard Kleinrock, developing a data-driven predictive model for technological growth, which received the Internet Research Initiative Prize."
+  },
+  {
+    title: "Pitching a Startup",
+    heroImage: "/scrapbook/posts/aecom.jpeg",
+    link: "https://drive.google.com/file/d/1qbBZv02p4GTkk-2wKvAqKD0nznNgN_9R/view?usp=sharing",
+    readMoreText: "WATCH THE PITCH",
     summary:
-      "I talk about WOmen in tech issues and the importance of diversity in tech. I also talk about the importance of responsible AI use.",
+      "I developed and pitched an innovation-prediction venture through MIT and Boston University venture accelerators. I learned to shape complex technical research into clear, compelling narratives that resonated with both technical and non-technical stakeholders. This reinforced my belief that technology only creates real impact when people understand and trust it!",
   }
 ];
